0fa88f4e1d8c577aa89c5c7872886b03e302a90d
4tsu <sho4tsuji@gmail.com>
2022-10-07 12:29:07 +0900

    ⚡️ 実行時間計測をエラー出力に

--- main.cpp
+++ main.cpp
@@ -22,6 +22,6 @@ int main(int argc, char **argv) {
     delete md;
     clock_t end = clock();
     const double time = static_cast<double>(end - start) / CLOCKS_PER_SEC*1000.0;
-    printf("time %lf[ms]\n", time);
+    fprintf(stderr, "time %lf[ms]\n", time);
 }
 // ========================================
