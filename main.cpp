3e787321f6a5e77e3fa9c0a92f87ff4055a7e4ad
4tsu <sho4tsuji@gmail.com>
2022-10-13 00:39:37 +0900

    🚧 デバッグ用にパラメータを変更

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
     setup_info(mi);
 
     MD *md = new MD(mi);
-    md->set_params(1000, 10, 0.0020); // int steps, int ob_interval, double dt
+    md->set_params(1, 10, 0.0020); // int steps, int ob_interval, double dt
     md->set_box(100, 10, 10, 3.5); // int N, double xl, double yl, double cutoff
     md->set_margin(0.5);
     md->run();
