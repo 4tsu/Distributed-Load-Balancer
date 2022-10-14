5d52f511c813cb6ca68d19943a429a52b2502c09
4tsu <sho4tsuji@gmail.com>
2022-10-14 22:07:36 +0900

    ⚡️ main()の時間計測表示を変更

--- main.cpp
+++ main.cpp
@@ -3,25 +3,28 @@
 // ========================================
 
 int main(int argc, char **argv) {
-    // 実行時間計測
-    clock_t start = clock();
-
     MPI_Init(&argc, &argv);
     MPIinfo mi;
     setup_info(mi);
 
+    // 実行時間計測
+    clock_t start = clock();
+
     MD *md = new MD(mi);
-    md->set_params(1000, 1, 0.0020); // int steps, int ob_interval, double dt
+    md->set_params(1000, 10, 0.0020); // int steps, int ob_interval, double dt
     md->set_box(100, 10, 10, 3.5); // int N, double xl, double yl, double cutoff
     md->set_margin(0.5);
     md->run();
-
-    MPI_Finalize();
+    delete md;
 
     // 実行時間計測
-    delete md;
     clock_t end = clock();
     const double time = static_cast<double>(end - start) / CLOCKS_PER_SEC*1000.0;
-    fprintf(stderr, "time %lf[ms]\n", time);
+    double avg_time;
+    MPI_Allreduce(&time, &avg_time, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    if (mi.rank == 0)
+        fprintf(stderr, "avg.time %lf[ms] (%d procs)\n", avg_time/static_cast<double>(mi.procs), mi.procs);
+
+    MPI_Finalize();
 }
 // ========================================
