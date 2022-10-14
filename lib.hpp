fd86c4be37942a0495a929c6bf235b9ca3eec61a
4tsu <sho4tsuji@gmail.com>
2022-10-14 17:34:44 +0900

    🔨 デバッグ用コードを追加

--- lib.hpp
+++ lib.hpp
@@ -15,3 +15,24 @@
 #include <string>
 
 #include <mpi.h>
+
+// ----------------------
+
+//デバッグ用
+inline void ckpt() {
+    static int c = 1;
+    fprintf(stderr, "c.%d\n", c);
+    c++;
+}
+inline void ckpt2() {
+    static int d = 1;
+    fprintf(stderr, "d.%d\n", d);
+    d++;
+}
+inline void ckpt3() {
+    static int e = 1;
+    fprintf(stderr, "e.%d\n", e);
+    e++;
+}
+
+// ----------------------
