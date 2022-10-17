7f3b32e0e4fdfda43334f4f7f87ec68f10462b79
4tsu <sho4tsuji@gmail.com>
2022-10-17 18:43:35 +0900

    🚨communicate_atoms()の通信バグあり
    粒子情報を受信するときか、受信バッファからクラスに書き込むときにずれているのでは

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ int main(int argc, char **argv) {
     clock_t start = clock();
 
     MD *md = new MD(mi);
-    md->set_params(1000, 10, 0.0020); // int steps, int ob_interval, double dt
-    md->set_box(100, 10, 10, 3.5); // int N, double xl, double yl, double cutoff
+    md->set_params(1, 10, 0.0020); // int steps, int ob_interval, double dt
+    md->set_box(10000, 100, 100, 3.5); // int N, double xl, double yl, double cutoff
     md->set_margin(0.5);
     md->run();
     delete md;
