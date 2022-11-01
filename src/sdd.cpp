a9c74742087fa332ae94bdb9089c0c3383870cca
4tsu labo <sho4tsuji@gmail.com>
2022-11-01 14:19:41 +0900

    🚧

--- src/sdd.cpp
+++ src/sdd.cpp
@@ -78,5 +78,53 @@ void Sdd::simple(Variables* vars, Systemparam* sysp, const MPIinfo &mi, SubRegio
     // 最初にAllgatherで移動粒子数のテーブルを共有（1次元で送って戻す）
     // 次に必要なペアでだけ通信をする
 
+    std::vector<unsigned long> num_migration(mi.procs);
+    for (int i=0; i<mi.procs; i++) {
+        num_migration.at(i) = migration_atoms.at(i).size();
+    }
+    std::vector<std::vector<unsigned long>> migration_table(mi.procs, std::vector<unsigned long>(mi.procs));
+    std::vector<unsigned long> recvbuf(mi.procs*mi.procs);
+    MPI_Allgather(num_migration.data(), mi.procs, MPI_UNSIGNED_LONG, recvbuf.data(), mi.procs, MPI_UNSIGNED_LONG, MPI_COMM_WORLD);
+    int bias = 0;
+    unsigned long sum_recv = 0;
+    for (auto& one_mt : migration_table) {
+        std::copy(recvbuf.begin()+bias, recvbuf.begin()+bias+mi.procs, one_mt.begin());
+        bias += mi.procs;
+        sum_recv += one_mt.at(mi.rank);
+    }
+
+for (auto a : migration_table) {
+    for (auto b : a) {
+        fprintf(stderr, "%lu ", b);
+    }
+    fprintf(stderr, "\n");
+}
+
+    MPI_Request ireq;
+    std::vector<MPI_Request> mpi_send_requests;
+    std::vector<MPI_Request> mpi_recv_requests;
+    unsigned long nm_size;
+    std::vector<Atom> recvbuf(sum_recv);
+    unsigned long recv_head = 0;
+    for (int p=0; p<mi.procs; p++) {
+        if (num_migration.at(p)!=0) {
+            nm_size = num_migration.at(p)*sizeof(Atom);
+            MPI_Isend(migration_atoms.at(p).data(), nm_size, MPI_CHAR, p, 0, MPI_COMM_WORLD, &ireq);
+            mpi_send_requests.push_back(ireq);
+        }
+        if (migration_table.at(p).size()!=0) {
+            nm_size = migration_table.at(p).size()*sizeof(Atom);
+            MPI_Irecv(&recvbuf.at(recv_head), nm_size, MPI_CHAR, p, 0, MPI_COMM_WORLD, &ireq);
+            mpi_recv_requests.push_back(ireq);
+        }
+    }
+    MPI_Status st;
+    for (auto& ireq : mpi_recv_requests) {
+        MPI_Wait(&ireq, &st);
+    }
+    for (auto& ireq : mpi_recv_requests) {
+        MPI_Wait(&ireq, &st);
+    }
+
 }
 // ============================================
