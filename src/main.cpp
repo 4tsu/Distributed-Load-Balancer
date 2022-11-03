5261e519425d55a14710ed2410e48777f9344cd9
4tsu <sho4tsuji@gmail.com>
2022-11-03 21:50:49 +0900

    ✏️ デフォルトパラメータを変更

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,10 @@ int main(int argc, char **argv) {
     clock_t start = clock();
 
     MD *md = new MD(mi);
-    md->set_params(1000, 10, 0.0020); // int steps, int ob_interval, double dt
-    md->set_box(2500, 50, 50, 3.5); // unsigned long N, double xl, double yl, double cutoff
+    md->set_params(2000, 20, 0.0020); // int steps, int ob_interval, double dt
+    md->set_box(1600, 40, 40, 3.5); // unsigned long N, double xl, double yl, double cutoff
     md->set_margin(0.5);
-    md->set_config("smpl2d.dump");
+    md->set_config("make");
     md->set_sdd(0);
     md->run();
     delete md;
