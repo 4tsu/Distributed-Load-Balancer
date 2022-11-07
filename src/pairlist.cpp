a1afb3f1a8cb9bd84e25edc6348aa972b62cdcba
4tsu <sho4tsuji@gmail.com>
2022-11-07 23:35:06 +0900

    🚧 メッシュ探索の途中

--- src/pairlist.cpp
+++ src/pairlist.cpp
@@ -81,4 +81,79 @@ void PairList::make_pair(Variables* vars, Systemparam* sysp) {
     vars->other_atoms = new_other_atom;
 }
 
+
+
+void PairList::set_mesh(Variables* vars, Systemparam* sysp) {
+    std::vector<double> limits;
+    limits = calc_limit(vars);
+    this->nmx = static_cast<int>((limits.at(1) - limits.at(0))/sysp->co_margin) - 1;
+    this->lmx = static_cast<double>(limits.at(1) - limits.at(0))/nmx;
+    this->nmy = static_cast<int>((limits.at(3) - limits.at(2))/sysp->co_margin) - 1;
+    this->lmy = static_cast<double>(limits.at(3) - limits.at(2))/nmy;
+    assert(nmx>2 && nmy>2);
+    assert(lmx>sysp->co_margin && lmy>sysp->co_margin);
+    this->num_mesh = nmx*nmy;
+    this->counter.resize(num_mesh);
+    this->head_index.resize(num_mesh);
+    this->sorted_index.resize(vars->number_of_atoms());
+}
+
+
+
+void PairList::set_index(Variables* vars) {
+    unsigned long pn = vars->number_of_atoms();
+    std::vector<unsigned long> position_buffer(pn);
+    for (int i=0; i<pn; i++) {
+        int ix = static_cast<int>(vars->atoms.at(i).x/lmx);
+        int iy = static_cast<int>(vars->atoms.at(i).y/lmy);
+        assert(0<=ix && ix<nmx);
+        assert(0<=iy && iy<nmy);
+        int im = ix + iy*nmx;
+        assert(0<=im && im<num_mesh);
+        counter.at(im)++;
+        position_buffer.at(i) = im;
+    }
+
+    head_index.at(0) = 0;
+    int sum = 0;
+    for (int i=1; i<num_mesh; i++) {
+        sum += counter.at(i-1);
+        head_index.at(i) = sum;
+    }
+
+    std::vector<unsigned long> indexes(num_mesh);
+    std::fill(indexes.begin(), indexes.end(), 0);
+    for (int i=0; i<pn; i++) {
+        int im = position_buffer.at(i);
+        int j = head_index.at(im) + indexes.at(im);
+        sorted_index.at(j) = i;
+        indexes.at(im)++;
+    }
+}
+
+
+
+void PairList::search(int im, Variables* vars, Systemparam* sysp) {
+    int ih = head_index.at(im);
+    int pnm = counter.at(im);
+    Atom *atoms = vars->atoms.data();
+    for (int m=ih; m<ih+pnm-1; m++) {
+        for (int n=m+1; n<ih+pnm; n++) {
+            int i = sorted_index.at(m);
+            int j = sorted_index.at(n);
+            double dx = atoms[j].x - atoms[i].x;
+            double dy = atoms[j].y - atoms[i].y;
+            periodic_distance(dx, dy, sysp);
+            double r = dx*dx + dy*dy;
+            if (r > sysp->co_margin)
+                continue;
+            Pair p;
+            p.i = i;
+            p.j = j;
+            p.idi = atoms[i].id;
+            p.idj = atoms[j].id;
+
+        }
+    }
+}
 // ============================================
--- src/pairlist.hpp
+++ src/pairlist.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "variables.hpp"
 #include "systemparam.hpp"
+#include "subregion.hpp"
 
 // ============================================
 
@@ -18,6 +19,14 @@ void set_pair(Pair &pair, unsigned long i, unsigned long j, unsigned long idi, u
 
 // ペアリストクラス
 class PairList {
+private:
+    void set_mesh(Variables*, Systemparam*);
+    void set_index(Variables*);
+    void search(int, Variables*, Systemparam*);
+    void search_other(int, int, Variables*, Systemparam*);
+    double lmx, lmy, nmx, nmy;
+    int num_mesh;
+    std::vector<unsigned long> counter, head_index, sorted_index;
 public:
     std::vector<Pair> list;
     std::vector<std::vector<Pair>> other_list;
