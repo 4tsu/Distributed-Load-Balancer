a69b46f321529e667394e5b82a247b4ff24b5727
4tsu labo <sho4tsuji@gmail.com>
2022-12-06 17:04:34 +0900

    🚧 3d化を進めた

--- src/systemparam.hpp
+++ src/systemparam.hpp
@@ -48,11 +48,13 @@ void periodic_distance(double &dx, double &dy, double &dz) {
 
 
 inline
-void periodic_coordinate(double &x, double &y) {
+void periodic_coordinate(double &x, double &y, double &z) {
     if (sysp::x_min > x)      x += sysp::xl;
     else if (sysp::x_max < x) x -= sysp::xl;
     if (sysp::y_min > y)      y += sysp::yl;
     else if (sysp::y_max < y) y -= sysp::yl;
+    if (sysp::z_min > z)      z += sysp::zl;
+    else if (sysp::z_max < z) z -= sysp::zl;
 }
 
 // ===================================================
